708c94ef3ad3626a70b422496a9e88f0a75468e6
Fabricyo Barreto <bass.barreto@gmail.com>
2021-01-05 21:03:24 -0300

    CardItem: Foi estilizado um icone de lixeira para remover o card e estilizado uma imagem para representar o titulo do card

--- src/components/CardItem/index.tsx
+++ src/components/CardItem/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { Alert } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
 
-import { Container, Title, Description } from './styles';
+import {
+  Container,
+  ContainerImage,
+  ContainerText,
+  Title,
+  Description,
+  TrashButton,
+} from './styles';
 
 const CardItem: React.FC = () => {
   return (
     <Container>
-      <Title>NodeJS</Title>
-      <Description>Aprender templates e RESTAPI</Description>
+      <ContainerImage>
+        <Feather name="image" size={30} />
+      </ContainerImage>
+
+      <ContainerText>
+        <Title>NodeJS</Title>
+        <Description>Aprender templates e RESTAPI</Description>
+      </ContainerText>
+
+      <TrashButton
+        onPress={() => {
+          Alert.alert('trash');
+        }}
+      >
+        <Feather name="trash-2" size={20} />
+      </TrashButton>
     </Container>
   );
 };
--- src/components/CardItem/styles.ts
+++ src/components/CardItem/styles.ts
@@ -1,18 +1,36 @@
 import styled from 'styled-components/native';
+import { lighten } from 'polished';
 
 export const Container = styled.View`
-  margin: 20px 35px 0 35px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px 25px 0 25px;
   border: 1px solid orange;
-  border-radius: 5px;
+  border-radius: 8px;
   background-color: orange;
   height: 90px;
-  padding: 20px;
+  padding: 15px;
 `;
 
+export const ContainerImage = styled.View`
+  background-color: ${lighten(0.15, 'orange')};
+  width: 55px;
+  height: 55px;
+  border-radius: 15px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContainerText = styled.View``;
+
 export const Title = styled.Text`
+  font-size: 18px;
   color: white;
 `;
 
 export const Description = styled.Text`
   color: #eee;
 `;
+
+export const TrashButton = styled.TouchableOpacity``;
